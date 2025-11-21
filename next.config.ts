94b020eb64b8552a936466e51779aa978ef443eb
Nick Sheffield <numbereft@gmail.com>
2025-11-21 14:48:53 +0000

    feat: try explicitly setting build directory

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: 'export', // <=== enables static exports
+	distDir: 'out',
 	reactStrictMode: true,
 }
 
