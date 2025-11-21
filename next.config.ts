ab3fb9dac8080a2e6e89b8c153a5dd07db3888cd
Nick Sheffield <numbereft@gmail.com>
2025-11-21 14:11:21 +0000

    feat: set output mode for next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	/* config options here */
+	output: 'export', // <=== enables static exports
+	reactStrictMode: true,
+}
 
-export default nextConfig;
+export default nextConfig
